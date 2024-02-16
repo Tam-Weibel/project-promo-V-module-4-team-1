fb92144fd86320b122fc4a4da9a129f8d2c64509
94Jenn <151082727+94Jenn@users.noreply.github.com>
2024-02-16 13:14:35 +0100

    hero done2

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,10 +5,11 @@ import '../scss/layout/Hero.scss';
 function Hero() {
   return (
     <div className="hero">
-      <h1 className="hero__title">🫸🏻 PROYECTOS MOLONES 🫷🏻</h1>
+      <h1 className="hero__title"> PROYECTOS MOLONES </h1>
       <p className="hero__text">
-      Vitrina digital que te inspira a través de la tecnología
+      Vitrina digital que te inspira a través de la tecnología.
       </p>
+
     </div>
   );
 }
