e0355bc9a6d34e6ceb620d61423a53adff334166
LauraSabrina90 <sanchezlaurasabrina@hotmail.com>
2024-02-26 14:24:05 +0100

    contact form no media queries

--- web/src/components/contact/ContactForm.jsx
+++ web/src/components/contact/ContactForm.jsx
@@ -1,39 +1,60 @@
-import '../../scss/layout/ContactForm.scss';
-import '../../scss/layout/Main.scss';
+import "../../scss/layout/ContactForm.scss";
+import "../../scss/layout/Main.scss";
 
 function ContactForm() {
   return (
-    <main className='main'>
-    <form action="https://formspree.io/f/xrgwndbn" method="POST" className="form">
+    <main className="main">
+      <form
+        action="https://formspree.io/f/xrgwndbn"
+        method="POST"
+        className="form"
+      >
         <div className="herocontact">
-            <h1 className="herocontact__title">PROYECTOS MOLONES</h1>
-            <p className="herocontact__text">Contacta con nosotras</p>
+          <h1 className="herocontact__title">PROYECTOS MOLONES</h1>
+          <p className="herocontact__text">Contacta con nosotras</p>
         </div>
 
-        <div className="">
-            <label htmlFor="name">Nombre completo <span className="">*</span></label>
-            <input className="" type="text" id="name" placeholder="Nombre..." required />
-        </div>
+        <label htmlFor="name" className="labelForm">
+          Nombre completo 
+        </label>
+        <input
+          className="inputForm"
+          type="text"
+          id="name"
+          placeholder="Nombre..."
+          required
+        />
 
-        <div className="">
-            <label htmlFor="email">Email <span className="">*</span></label>
-            <input className="" type="email" id="email" placeholder="nombre.apellidos@mail.com" required />
-        </div>
+        <label htmlFor="email"  className="labelForm">
+          Email 
+        </label>
+        <input
+          className="inputForm"
+          type="email"
+          id="email"
+          placeholder="nombre.apellidos@mail.com"
+          required
+        />
 
-        <div className="">
-            <label htmlFor="number">Teléfono</label>
-            <input className="" type="tel" id="number" placeholder="Ej:123456789" />
-        </div>
+        <label htmlFor="number"  className="labelForm">Teléfono</label>
+        <input className="inputForm" type="tel" id="number" placeholder="Ej:123456789" />
 
-        <div className="">
-            <label htmlFor="message">Mensaje <span className="">*</span></label>
-            <textarea className="" name="message" id="message" cols="30" rows="10" placeholder="¿Qué necesitas?"></textarea> 
-            
-        </div>
-        <input className="" type="submit" value="Enviar"></input>
-    </form>
+        <label htmlFor="message"  className="labelForm">
+          Mensaje 
+        </label>
+        <textarea
+          className="textArea"
+          name="message"
+          id="message"
+          cols="30"
+          rows="10"
+          placeholder="¿Qué necesitas?"
+        ></textarea>
+
+        <input className="btn btnForm" type="submit" value="Enviar"></input>
+      </form>
     </main>
-  )
+  );
 }
 
-export default ContactForm
+export default ContactForm;
