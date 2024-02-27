de021d84316403a398c8a53cab40f67d4a816099
Tam-Weibel <tamaraweibel.comgit config --global user.email tamaraweibel@gmail.com>
2024-02-27 12:46:36 +0100

    variable port

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const mysql = require('mysql2/promise');
 const server = express();
 require('dotenv').config();
 server.use(cors())
-const port = 5001;
+const port = process.env.PORT || 5001;
 
 server.use(express.json({limit: '25mb'}))
 server.set("view engine", "ejs");
