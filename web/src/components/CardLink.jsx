c54952b80951361560dcc23a63fa28eee0ee7e43
Tam-Weibel <tamaraweibel.comgit config --global user.email tamaraweibel@gmail.com>
2024-02-29 09:58:59 +0100

    started linkedin share

--- web/src/components/CardLink.jsx
+++ web/src/components/CardLink.jsx
@@ -1,12 +1,16 @@
 import '../scss/layout/CardLink.scss';
 import PropTypes from 'prop-types';
 
-function CardLink({cardLink, hideCardLink}) {
+function CardLink({ cardLink, hideCardLink }) {
   let showLoader = cardLink === '' ? '' : 'hidden';
   let showMessage = cardLink !== '' ? '' : 'hidden';
+  let url = encodeURIComponent(cardLink);
   return (
     <section className={`cardLink ${hideCardLink}`}>
-      <span className={`cardLink__text ${showMessage}`}> La tarjeta ha sido creada:</span>
+      <span className={`cardLink__text ${showMessage}`}>
+        {' '}
+        La tarjeta ha sido creada:
+      </span>
       <span className={`cardLink__loader ${showLoader}`}></span>
       <a
         href={cardLink}
@@ -16,12 +20,25 @@ function CardLink({cardLink, hideCardLink}) {
       >
         <strong className='cardLink__url'>{cardLink}</strong>
       </a>
+      {/* <a
+        target='_blank'
+        className='c-black'
+        rel='noreferrer'
+        href={`https://www.linkedin.com/sharing/share-offsite/?url=${url}`}
+      >
+        Share on LinkedIn
+      </a>
+      <a
+        target='_blank'
+        title='Share on LinkedIn'
+        href={`http://www.linkedin.com/shareArticle?mini=true&url=${url}`}
+      >Second share option</a> */}
     </section>
   );
 }
 
 CardLink.propTypes = {
-  cardLink : PropTypes.string,
+  cardLink: PropTypes.string,
   hideCardLink: PropTypes.string.isRequired,
 };
 export default CardLink;
