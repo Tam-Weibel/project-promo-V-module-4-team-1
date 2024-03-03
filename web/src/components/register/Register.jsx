aa885c23b935b0f6b512b53f2a75c8705d56961f
Tam-Weibel <tamaraweibel.comgit config --global user.email tamaraweibel@gmail.com>
2024-03-03 17:03:33 +0100

    save token to ls on registration

--- web/src/components/register/Register.jsx
+++ web/src/components/register/Register.jsx
@@ -40,6 +40,7 @@ function Register({ setLoggedIn, setUserName }) {
         setLoggedIn(true);
         setUserName(signInData.username);
         localStorage.set('user name', signInData.username);
+        localStorage.set('token', response.token);
       } else {
         setLoggedIn(false);
       }
